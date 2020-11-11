0a5f0e1eb976c0bacc8b6d25fac9ceef3861553d
Sushil Gupta <susgupta@redhat.com>
2020-11-12 01:58:34 +0530

    Add solutions

--- strings/isAnagram.go
+++ strings/isAnagram.go
@@ -1,3 +1,15 @@
+/*
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+*/
 func SortString(w string) string {
 	s := strings.Split(w, "")
 	sort.Strings(s)
@@ -8,8 +20,14 @@ func isAnagram(s string, t string) bool {
 	t1 := SortString(s)
 	t2 := SortString(t)
 
-    if t1 != t2 {
-        return false
-    }
-    return true
+	if len(t1) != len(t2) {
+		return false
+	} else {
+		for i := 0; i < len(t1); i++ {
+			if t1[i] != t2[i] {
+				return false
+			}
+		}
+		return true
+	}
 }
